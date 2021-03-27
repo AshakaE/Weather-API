d35ea3b759ce0b31c3e5ca35416b304396fd5e8e
AshakaE <ashakaegerega@gmail.com>
2021-03-27 12:45:43 +0100

    Add json jbuilder for request output

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,1 +1,13 @@
-class
+class Api::V1::LocationsController < ApiController
+  before_action :set_location
+
+  def show
+
+  end
+
+  private
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
+end
