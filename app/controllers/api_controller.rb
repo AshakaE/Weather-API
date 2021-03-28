0aa39d0dc32dce8f4faa425246102cdc09701d3c
AshakaE <ashakaegerega@gmail.com>
2021-03-28 10:03:56 +0100

    Add devise and jwt

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,24 @@
 class ApiController < ApplicationController
   before_action :set_default_format
-
+  before_action :authenticate_token!
+  
   private
   
-  def set_default_format
-    request.format = :json
-  end
+    def set_default_format
+      request.format = :json
+    end
+
+    def authenticate_token!
+      payload = JsonWebToken.decode(auth_token)
+      if payload.present?
+        @current_user = User.find(payload['sub'])
+        p @current_user
+      else
+        render json: {errors: ['Invalind auth token']}, status: :unanthorized
+      end
+    end
+
+    def auth_token
+      @auth_token ||= request.headers.fetch('Authorization', '').split(' ').last
+    end
 end
